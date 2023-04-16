f8ac0c6bfc53ccc5f926843bace82f0dd145ec71
bl3rt4 <blerta.aazemi@gmail.com>
2023-04-16 15:21:25 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         random.nextBytes(keyStream);
         return keyStream;
     }
+
     public static byte[] xor(byte[] text, byte[] keyStream) {
         byte[] returnText = new byte[text.length];
         for (int i = 0; i < text.length; i++) {
@@ -15,7 +16,32 @@ public class Main {
         return returnText;
     }
 
+
+
+
+
+
+
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String plaintext = "Hello, world!";
+        String key = "why_is_this_key_different";
+        byte[] encryptedText = symmetricEncrypt(plaintext, key);
+        System.out.println("Encrypted message: " + bytesToHex(encryptedText));
+
+        String decryptedText = symmetricDecrypt(encryptedText, key);
+        System.out.println("Decrypted message: " + decryptedText);
     }
+
+    public static String bytesToHex(byte[] bytes) {
+        String sb = "";
+        for (byte b : bytes) {
+            sb += (String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
 }
+
+
+
